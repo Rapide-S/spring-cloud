afdd370fad93c95faf1a72359c7df3cc5b4456d9
Rapide-S <2453697442@qq.com>
2019-06-27 17:47:20 +0800

    实时监控

---
+++ sp08-hystrix-dashboard/src/main/java/com/tedu/sp08/Sp08HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.tedu.sp08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableDiscoveryClient    //启用服务注册发现
+@EnableHystrixDashboard			//启用仪表盘
+@SpringBootApplication
+public class Sp08HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp08HystrixDashboardApplication.class, args);
+	}
+
+}
